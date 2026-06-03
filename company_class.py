16423384e1f3a5e7a183239f719b783f53ad322b
Cypress <kad2321@yahoo.com>
2026-06-03 14:22:00 -0400

    Created .gitignore and multiple classes

---
+++ company_class.py
@@ -0,0 +1,39 @@
+from employee_class import Employee
+
+class Company:
+    def __init__(self):
+        self.employees=[]
+
+    def add_employee(self, new_employee):
+        self.employees.append(new_employee)
+    
+    def display_employees(self):
+        print("Current Employees:")
+        for i in self.employees:
+            print(i.fname, i.lname)
+        print('----------')
+
+    def pay_employees(self):
+        print('Paying Employees')
+        for i in self.employees:
+            print("Paycheck for", i.fname, i.lname)
+            print(f"Amount: ${ i.calculate_paycheck():,.2f}") #Look up formatting
+            print("----------")
+
+
+
+
+def main():
+    my_company = Company()
+
+    employee1 = Employee('Cy', 'Doherty', 63000)
+    my_company.add_employee(employee1)
+    employee2 = Employee('Allyson', 'Langhoff', 75000)
+    my_company.add_employee(employee2)
+    employee3 = Employee('Lisa', 'Doherty', 59000)
+    my_company.add_employee(employee3)
+
+    my_company.display_employees()
+    my_company.pay_employees()
+
+main()
